7d9cb159c78fa7d3e5c3f69e99dabbd562906322
James Lucid <77985107+jl371@users.noreply.github.com>
2024-11-05 09:03:05 -0700

    fix of no main

--- codegenerator.cpp
+++ codegenerator.cpp
@@ -64,16 +64,11 @@ int main() {
     for (int i = 0; i < firstclass.size(); i++)
     {
         
-        vector<ParseTreeNode*> second = firstclass[i]->getChildren(); //this is the tree of each function declaration
-        for (int j = 0; j < second.size(); j++)
-        {
-            if (second[j]->name == "main") 
+        if (firstclass[i]->data == "main") 
             {
                 maincounter++;
-                mainfuncnode = second[j];
+                mainfuncnode = firstclass[i];
             }
-            
-        }
     }
     if (maincounter == 0) {
         cout << "Syntax error: No main function.\n";
